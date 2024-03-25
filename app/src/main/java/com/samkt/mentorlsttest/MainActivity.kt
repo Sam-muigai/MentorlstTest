df7090652c8ee4abb19a30e57b56533c4d624f7a
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-03-25 21:07:40 +0300

    Initial commit

---
+++ app/src/main/java/com/samkt/mentorlsttest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.samkt.mentorlsttest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.samkt.mentorlsttest.ui.theme.MentorlstTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MentorlstTestTheme {
+            }
+        }
+    }
+}
