f747fa7c586cfdd3ab9b3e0f1c931a4d0c4de438
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-03-26 10:25:07 +0300

    Add sign up screen preview

--- app/src/main/java/com/samkt/mentorlsttest/screens/signUp/SignUpScreen.kt
+++ app/src/main/java/com/samkt/mentorlsttest/screens/signUp/SignUpScreen.kt
@@ -18,9 +18,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.samkt.mentorlsttest.R
 import com.samkt.mentorlsttest.screens.components.MentorlistCheckBox
@@ -29,10 +29,13 @@ import com.samkt.mentorlsttest.screens.components.MentorlstButton
 import com.samkt.mentorlsttest.screens.components.MentorlstPasswordTextField
 import com.samkt.mentorlsttest.screens.components.MentorlstTextField
 import com.samkt.mentorlsttest.screens.navigation.AppScreens
+import com.samkt.mentorlsttest.ui.theme.MentorlstTestTheme
 
 @Composable
-fun SignUpScreen(signUpViewModel: SignUpViewModel = viewModel(),
-                 navController: NavHostController) {
+fun SignUpScreen(
+    signUpViewModel: SignUpViewModel = viewModel(),
+    navController: NavHostController,
+) {
     val signUpScreenState = signUpViewModel.signUpScreenState.collectAsState().value
 
     SignUpScreenContent(
@@ -75,16 +78,16 @@ fun SignUpScreenContent(
 ) {
     Scaffold(
         modifier =
-        modifier
-            .fillMaxSize()
-            .padding(12.dp),
+            modifier
+                .fillMaxSize()
+                .padding(12.dp),
     ) { paddingValues ->
         Column(
             modifier =
-            Modifier
-                .padding(paddingValues)
-                .verticalScroll(rememberScrollState())
-                .fillMaxSize(),
+                Modifier
+                    .padding(paddingValues)
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize(),
         ) {
             Spacer(modifier = Modifier.height(16.dp))
             Text(
@@ -172,9 +175,13 @@ fun SignUpScreenContent(
     }
 }
 
-// @Preview(showSystemUi = true)
-// @Composable
-// fun SignUpScreenPreview() {
-//    MentorlstTestTheme {
-//    }
-// }
+@Preview(showSystemUi = true)
+@Composable
+fun SignUpScreenPreview() {
+    MentorlstTestTheme {
+        SignUpScreenContent(
+            signUpScreenState = SignUpScreenState(),
+            onEvent = {},
+        )
+    }
+}
